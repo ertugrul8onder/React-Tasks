63fa09db990d7651069882f83cccbc9ee8971b1c
Ertuğrul Önder <ertugrul8onder@gmail.com>
2024-12-19 21:11:10 +0300

    form

--- 1.Form/src/App.jsx
+++ 1.Form/src/App.jsx
@@ -1,6 +1,59 @@
 function App() {
   return (
-    <></>
+    <div className="container vh-100">
+      <div className="row justify-content-center align-items-center h-100">
+        <div className="col-md-5">
+          <form className="row border border-white g-3 p-3">
+            <div className="col-12">
+              <label className="form-label fw-semibold fs-5">Purpose</label>
+              <div>
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="radio" name="purpose" id="client" value="1" />
+                  <label className="form-check-label" htmlFor="client">Client</label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="radio" name="purpose" id="group" value="2" />
+                  <label className="form-check-label" htmlFor="group">Group</label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="radio" name="purpose" id="agent" value="3" />
+                  <label className="form-check-label" htmlFor="agent">Agent</label>
+                </div>
+              </div>
+            </div>
+            <div className="col-12">
+              <label htmlFor="label" className="form-label fw-semibold fs-5">Label</label>
+              <input type="text" className="form-control border border-danger" id="label" name="label" aria-describedby="label_help" />
+              <div id="label_help" className="form-text text-white-50">Please write to label to here</div>
+            </div>
+            <div className="col-12">
+              <label htmlFor="key" className="form-label fw-semibold fs-5">Key</label>
+              <input type="text" className="form-control border border-danger" id="key" name="key" />
+              <div id="label_help" className="form-text text-white-50">Please write to key to here</div>
+            </div>
+            <div className="col-12">
+              <label className="form-label fw-semibold fs-5">Status</label>
+              <div>
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="radio" name="status" id="active" value="4" />
+                  <label className="form-check-label" htmlFor="active">Active</label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="radio" name="status" id="passive" value="5" />
+                  <label className="form-check-label" htmlFor="passive">Passive</label>
+                </div>
+              </div>
+            </div>
+            <button type="button" className="btn btn-success">
+              <div className="d-flex align-items-center justify-content-center">
+                <span className="fs-5">Save</span>
+                <i className="bi bi-check-lg" style={{ fontSize: "1.5rem" }}></i>
+              </div>
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
